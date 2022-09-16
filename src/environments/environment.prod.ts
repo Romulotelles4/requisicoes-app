93ae2c54dd13ae61ed2addc0987bb67ac3a34736
Romulo Carneiro <romulocarneiro2002@hotmail.com>
2022-09-16 15:28:19 -0300

    Add firebase no environment prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB3twisR2wghnyjqOdWi7yl3aIqSmV5Luk",
+    authDomain: "requisicoes-app-b168b.firebaseapp.com",
+    projectId: "requisicoes-app-b168b",
+    storageBucket: "requisicoes-app-b168b.appspot.com",
+    messagingSenderId: "505460409843",
+    appId: "1:505460409843:web:663c3900dc76f51be0be86"
+  }
 };
